4d156d47e20ea6503fb306e3f9f24e966cdec718
Jeff Kim <kim2jy@hotmail.com>
2020-08-27 15:15:58 -0700

    Initial Commit
    Co-authored-by: Ian Gregg <iangregg188@gmail.com>
    Co-authored-by: Kyle Hubbard <kyle.james.hubbard@gmail.com>
    Co-authored-by: Sean Downs <spdowns@protonmail.com>
    Co-authored-By: Cody Fritz <clanalia55@gmail.com>

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public string exitGame;
+    public string restartGame;
+    public GameObject theMenu;
+    // Start is called before the first frame update
+    void Start()
+    {
+       theMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(theMenu.activeInHierarchy)
+            {
+                theMenu.SetActive(false);
+                Time.timeScale = 1;
+
+            }
+            else
+            {
+                theMenu.SetActive(true);
+                Time.timeScale = 0;
+
+            }
+        }
+    }
+    public void ResumeGame()
+    {
+        theMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene(exitGame);
+    }
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(restartGame);
+    }
+}
---
+++ Assets/StageSelect2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StageSelect2 : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(3);
+    }
+}
